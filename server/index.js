e56473200417f9879505e40fd84ae8337d733a99
abhii093 <142203911+abhii093@users.noreply.github.com>
2025-09-09 23:38:29 +0530

    link updated

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://digitalmarket-drab.vercel.app",
+  "https://digital-marketing-agency-eight.vercel.app",
 ];
 
 app.use(
